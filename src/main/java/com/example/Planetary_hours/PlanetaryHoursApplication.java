ec239f2eecbb1881ea9ac400385b6dfa24886217
fkfkfk112233 <kk60607kk@gmail.com>
2026-08-12 02:53:44 +0800

    create a spring-boot project

---
+++ src/main/java/com/example/Planetary_hours/PlanetaryHoursApplication.java
@@ -0,0 +1,13 @@
+package com.example.Planetary_hours;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanetaryHoursApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanetaryHoursApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Planetary_hours/PlanetaryHoursApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Planetary_hours;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanetaryHoursApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
