6a4751eccdd97cdf89cef034b59ebd33604227c7
fkfkfk112233 <kk60607kk@gmail.com>
2026-08-12 21:55:08 +0800

    Update SunDataProvider.java

--- src/main/java/com/example/Planetary_hours/service/SunDataProvider.java
+++ src/main/java/com/example/Planetary_hours/service/SunDataProvider.java
@@ -3,8 +3,9 @@ package com.example.Planetary_hours.service;
 import java.time.LocalDate;
 
 import com.example.Planetary_hours.dto.SunData;
+import com.example.Planetary_hours.model.Location;
 
 public interface SunDataProvider {
 
-	SunData getSunData(LocalDate date);
+	SunData getSunData(LocalDate date, Location location);
 }
