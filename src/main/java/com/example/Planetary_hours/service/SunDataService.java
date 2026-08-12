d8c7e823e19d056cce8051a042fc323977519f50
fkfkfk112233 <kk60607kk@gmail.com>
2026-08-12 22:06:36 +0800

    Update SunDataService.java

--- src/main/java/com/example/Planetary_hours/service/SunDataService.java
+++ src/main/java/com/example/Planetary_hours/service/SunDataService.java
@@ -5,18 +5,39 @@ import java.time.LocalTime;
 
 import org.springframework.stereotype.Service;
 
+import com.example.Planetary_hours.calculator.SunCalculator;
 import com.example.Planetary_hours.dto.SunData;
 import com.example.Planetary_hours.model.Location;
 
 @Service
 public class SunDataService implements SunDataProvider{
 
-	@Override
-    public SunData getSunData(LocalDate date, Location location) {
+    private final SunCalculator sunCalculator;
 
-        // 暫時使用測試資料
-        LocalTime sunrise = LocalTime.of(5, 30);
-        LocalTime sunset = LocalTime.of(18, 30);
+    public SunDataService(
+            SunCalculator sunCalculator) {
+
+        this.sunCalculator = sunCalculator;
+    }
+
+    @Override
+    public SunData getSunData(
+            LocalDate date,
+            Location location) {
+
+        LocalTime sunrise =
+                sunCalculator.calculateSunrise(
+                        date,
+                        location.getLatitude(),
+                        location.getLongitude()
+                );
+
+        LocalTime sunset =
+                sunCalculator.calculateSunset(
+                        date,
+                        location.getLatitude(),
+                        location.getLongitude()
+                );
 
         return new SunData(
                 sunrise,
