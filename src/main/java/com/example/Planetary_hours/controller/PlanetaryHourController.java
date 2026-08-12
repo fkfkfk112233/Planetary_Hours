cd876626e712cab2d5232a5e6e8774e6467db182
fkfkfk112233 <kk60607kk@gmail.com>
2026-08-13 01:56:25 +0800

    add swagger

--- src/main/java/com/example/Planetary_hours/controller/PlanetaryHourController.java
+++ src/main/java/com/example/Planetary_hours/controller/PlanetaryHourController.java
@@ -12,7 +12,15 @@ import com.example.Planetary_hours.dto.PlanetaryHoursResponse;
 import com.example.Planetary_hours.model.Location;
 import com.example.Planetary_hours.service.PlanetaryHourService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RestController
+@Tag(
+	    name = "Planetary Hours",
+	    description = "Planetary Hours calculation API"
+	)
 public class PlanetaryHourController {
 
     private final PlanetaryHourService planetaryHourService;
@@ -24,8 +32,22 @@ public class PlanetaryHourController {
     }
 
     @GetMapping("/api/planetary-hours")
+    @Operation(
+            summary = "Calculate planetary hours",
+            description =
+                "Calculate 24 planetary hours "
+                + "for a specific date and location."
+        )
     public PlanetaryHoursResponse calculate(
+            @Parameter(
+                    description = "Date to calculate",
+                    example = "2026-08-13"
+                )
             @RequestParam LocalDate date,
+            
+            @Parameter(
+                    description = "Location"
+                )
             @RequestParam Location location) {
 
         return planetaryHourService.calculate(date, location);
