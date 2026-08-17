e6ddeef460ff4a9c82171215b70c822315a7a0dd
fkfkfk112233 <kk60607kk@gmail.com>
2026-08-17 16:52:07 +0800

    Create ActivityRepository.java

---
+++ src/main/java/com/example/Planetary_hours/repository/ActivityRepository.java
@@ -0,0 +1,9 @@
+package com.example.Planetary_hours.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.Planetary_hours.model.Activity;
+
+public interface ActivityRepository extends JpaRepository<Activity, Long>{
+
+}
