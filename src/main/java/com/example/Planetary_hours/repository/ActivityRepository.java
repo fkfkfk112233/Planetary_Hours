efc5a1caf0335e94ca0d4374a322345681eae207
fkfkfk112233 <kk60607kk@gmail.com>
2026-08-17 17:23:20 +0800

    Update ActivityRepository.java

--- src/main/java/com/example/Planetary_hours/repository/ActivityRepository.java
+++ src/main/java/com/example/Planetary_hours/repository/ActivityRepository.java
@@ -1,6 +1,7 @@
 package com.example.Planetary_hours.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -14,4 +15,8 @@ public interface ActivityRepository extends JpaRepository<Activity, Long>{
 
     // 查詢指定 Planet 且尚未被軟刪除的 Activity
     List<Activity> findByPlanetAndDeletedFalse(Planet planet);
+    
+    // 用 Planet + Activity 名稱尋找資料
+    // 用來避免初始化資料重複建立
+    Optional<Activity> findByPlanetAndName(Planet planet, String name);
 }
